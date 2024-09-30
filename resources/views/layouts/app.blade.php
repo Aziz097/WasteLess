93b6b1748f96ccdb71fd366b9230e768513c397a
Rizki Alfariz Ramadhan <ralfariz780@gmail.com>
2024-09-30 08:02:36 +0700

    refactor: deleting file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     
-
     <style>
         /* Untuk tata letak dan style utama */
         body, html {

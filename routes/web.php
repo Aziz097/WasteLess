74d16375004c05a5eb59224acea4de6cb8035876
Rizki Alfariz Ramadhan <ralfariz780@gmail.com>
2024-10-07 19:19:23 +0700

    refactor: cod and file

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\SupermarketController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductImageController;
-use Illuminate\Support\Facades\Auth;
 
 
 // Landing Page and Authentication Routes
@@ -86,9 +83,3 @@ Route::get('/supermarket/donasi/succes', function () {
 Route::get('/supermarket/produk', function () {
     return view('supermarkethomepage.produk');
 });
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::post('products/{product}/archive', [ProductController::class, 'archive'])->name('products.archive');
-Route::delete('products/images/{image}', [ProductImageController::class, 'destroy'])->name('products.images.destroy');
-Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');

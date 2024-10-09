230d2f6b82c37fe80e191c85596c1b033277202b
zidbytes <143333408+zidbytes@users.noreply.github.com>
2024-10-09 13:22:10 +0700

    Revert "update: mengubah seluruh nama "products" menjadi "product""

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search Product</title>
+    <title>Search Products</title>
 </head>
 <body>
     <form action="{{ route('search') }}" method="GET">
-        <input type="text" name="search" placeholder="Search Product" value="{{ request('search') }}">
+        <input type="text" name="search" placeholder="Search Products" value="{{ request('search') }}">
         <button type="submit">Search</button>
     </form>
     
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/search', [ProductController::class, 'index'])->name('search');
+Route::get('/search', [ProductsController::class, 'index'])->name('search');
 
-Route::get('/searchfilter', [ProductController::class, 'search'])->name('search');
+Route::get('/searchfilter', [ProductsController::class, 'search'])->name('search');

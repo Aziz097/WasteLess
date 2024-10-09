d373497f8a02bbc59d0a3fd8929a8e89bbe6cfa8
zidan-rhn <zraihan0111@gmail.com>
2024-10-09 09:05:03 +0700

    update: membatalkan merge

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/search', [ProductsController::class, 'index'])->name('search');
-
-Route::get('/searchfilter', [ProductsController::class, 'search'])->name('search');
